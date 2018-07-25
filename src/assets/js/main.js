c584f78ba0e96475dfc49ce768abb960e758dda8
Evelyn Galvez <evelyn.galvez.p@gmail.com>
2018-07-25 11:30:38 -0400

    splash básico con función timeout

---
+++ src/assets/js/main.js
@@ -0,0 +1,14 @@
+function splash() {
+  document.getElementById('splash').style.display = 'block';
+  document.getElementById('foodmap').style.display = 'none';
+}
+
+function footmap() {
+  document.getElementById('splash').style.display = 'none';
+  document.getElementById('foodmap').style.display = 'block';
+}
+
+
+window.onload = function() {
+  setTimeout(footmap, 4000);
+};
